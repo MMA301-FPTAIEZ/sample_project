b8bbcf8f0d4f4d0e440c31c7052d319aaab0c301
hoshikira18 <khuyen.dev183@gmail.com>
2025-07-12 19:37:41 +0700

    init

---
+++ navigators/RootNavigator.js
@@ -0,0 +1,28 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomePage from '../screens/HomePage';
+
+const Tabs = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const BottomTabs = () => {
+  return (
+    <Tabs.Navigator>
+      <Tabs.Screen name="Home" component={HomePage} />
+    </Tabs.Navigator>
+  );
+};
+
+const RootNavigator = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        animation: 'scale_from_center',
+      }}
+    >
+      <Stack.Screen name="Tabs" component={BottomTabs} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
+};
+
+export default RootNavigator;
